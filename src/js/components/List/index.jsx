bcf808e73e259aeedeac7daf41ea98030bba8e14
Kevin Farrugia <hello@imkev.dev>
2021-05-19 13:10:10 +0200

    Removed index key from list iterator

--- src/js/components/List/index.jsx
+++ src/js/components/List/index.jsx
@@ -9,6 +9,7 @@ const LOREM_IPSUM =
 const List = () => {
   const [list, setList] = useState([
     {
+      id: 0,
       img: 0,
       text: LOREM_IPSUM,
     },
@@ -19,6 +20,7 @@ const List = () => {
     setTimeout(() => {
       setList([
         {
+          id: Math.random() * 100000000,
           img: Math.ceil(Math.random() * 8),
           text: LOREM_IPSUM.slice(Math.ceil(Math.random() * 100)),
         },
@@ -31,9 +33,9 @@ const List = () => {
     <section>
       <ul id="list" className="mx-auto max-w-sm p-4 space-y-4">
         {list &&
-          list.map((n, index) => (
+          list.map((n) => (
             // eslint-disable-next-line react/no-array-index-key
-            <ListItem key={index} img={n.img} text={n.text} />
+            <ListItem key={n.id} img={n.img} text={n.text} />
           ))}
       </ul>
     </section>
